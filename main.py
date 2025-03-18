eda9d25440864f147a779db51b50cbf3f0717867
PukhalskyiDmytro <pukhalskyi@knu.ua>
2025-03-18 13:09:03 +0200

    4

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import turtle
+import time
 
 class Dial:
-    def __init__(self):
+    def __init__(self, t):
         self.smallHand = Hand()
         self.bigHand =  Hand()
 
         self.hours = 0
         self.minutes = 0
+        self.digits = [Digit() for i in range (0, 11)]
+        self.t = t
 
         while True:
-            pass
+            self.draw()
+            time.sleep(t)
+
+    def draw(self):
+        self.draw()
+        self.smallHand.draw(self.hours)
+        self.bigHand.draw(self.minutes)
+        for i in range(0, 11):
+            self.digits[i].draw(i+1)
+
+
+    def tick(self):
+        pass
